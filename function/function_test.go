af23c5099f7ef2bfb8ac1a21183ec04b5ad4a5ff
Nozima <nozimamingboyeva@gmail.com>
2024-09-18 20:54:11 +0500

    3homework

---
+++ function/function_test.go
@@ -0,0 +1,40 @@
+package function_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/nozimamax/homeworks/function"
+)
+
+type Config struct {
+	AppName string `json:"app_name" yaml:"app_name"`
+	Port    int    `json:"port" yaml:"port"`
+}
+
+func TestLoadJSONConfig(t *testing.T) {
+	expectedConfig := Config{AppName: "TestApp", Port: 8080}
+
+	err := function.LoadJSONConfig("../testdata/config.json", &expectedConfig)
+	assert.NoError(t, err)
+	assert.Equal(t, "TestApp", expectedConfig.AppName)
+	assert.Equal(t, 8080, expectedConfig.Port)
+}
+
+func TestLoadYAMLConfig(t *testing.T) {
+	expectedConfig := Config{AppName: "TestApp", Port: 8080}
+
+	err := function.LoadYAMLConfig("../testdata/config.yaml", &expectedConfig)
+	assert.NoError(t, err)
+	assert.Equal(t, "TestApp", expectedConfig.AppName)
+	assert.Equal(t, 8080, expectedConfig.Port)
+}
+
+func TestLoadENVConfig(t *testing.T) {
+	err := function.LoadENVConfig("../testdata/.env")
+	assert.NoError(t, err)
+
+	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
+	assert.Equal(t, "8080", os.Getenv("PORT"))
+}
